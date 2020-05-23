1f429f0f6917942fb20847ef8e762be4d0e4d974
Eden Park <2015015407@student.sit.ac.nz>
2020-05-23 15:00:52 +1200

    first commit

---
+++ 6-zigzag.py
@@ -0,0 +1,8 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        
+if __name__ == "__main__":
+    Solution.convert("PAYPALISHIRING",3);
+    # output: PAHNAPLSIIGYIR
+    Solution.convert("PAYPALISHIRING",4);
+    # output: PINALSIGYAHRPI
