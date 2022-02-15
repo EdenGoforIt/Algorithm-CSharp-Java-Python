02e36d594f6e931c9f9470799b68b44de7aeeb3a
EdenPark <edengoforit@gmail.com>
2022-02-15 19:46:04 +1300

    find matching string

---
+++ sparseArray.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY strings
+#  2. STRING_ARRAY queries
+#
+
+
+def matchingStrings(strings, queries):
+    result = []
+
+    for s in queries:
+        numbers = strings.count(s)
+        result.append(numbers)
+
+    print(result)
+
+
+if __name__ == '__main__':
+    matchingStrings(['ab', 'ab', 'abc'], ['ab', 'abc', 'bc'])
