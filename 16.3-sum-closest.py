4da4fab2b4d4c42325100f1b31fae46550341c51
EdenPark <edengoforit@gmail.com>
2022-03-15 20:45:19 +1300

    ThreeSum

---
+++ 16.3-sum-closest.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=16 lang=python3
+#
+# [16] 3Sum Closest
+#
+from typing import List
+# @lc code=start
+
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        return 3
+
+
+print(Solution().threeSumClosest([-1, 2, 1, -4], 1))
+
+# @lc code=end
--- three_sum.py
+++ three_sum.py
@@ -3,10 +3,32 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = []
+        length = len(nums)
+        for left in range(length - 2):
+            # avoid duplication
+            if left > 0 and nums[left] == nums[left-1]:
+                continue
+            mid = left + 1
+            right = length - 1
+            while mid < right:
+                sum = nums[left] + nums[mid] + nums[right]
+                if sum < 0:
+                    mid += 1
+                elif sum > 0:
+                    right -= 1
+                else:
+                    result.append([nums[left], nums[mid], nums[right]])
+                    while mid < right and nums[mid] == nums[mid+1]:
+                        mid += 1
+                    while mid < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    mid += 1
+                    right -= 1
+        return result
 
 
 if __name__ == '__main__':
-    test1 = Solution().threeSum([-1, 0, 1, 2, -1, -4])
-    test2 = Solution().threeSum([0])
+    test1 = Solution().threeSum([-2, 0, 0, 2, 2])
     print(test1)
-    print(test2)
