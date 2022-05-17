e23a15d2a3a16665267dedff9610e888baa28e02
EdenPark <edengoforit@gmail.com>
2022-05-17 21:09:37 +1200

    Sum closest

--- 16.3-sum-closest.py
+++ 16.3-sum-closest.py
@@ -2,17 +2,34 @@
 # @lc app=leetcode id=16 lang=python3
 #
 # [16] 3Sum Closest
-#
-from typing import List
+# 
+# from typing import List
 # @lc code=start
 
 
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        return 3
+        length = len(nums)
+        sum = nums[0] + nums[1] + nums[2]
+        nums.sort()
+
+        for i in range(length - 2):
+            pivot = nums[i]
+            start = i + 1
+            end = length-1
+
+            while (start < end):
+                current = pivot + nums[start] + nums[end]
+                if current < target:
+                    start += 1
+                else:
+                    end -= 1
+                if abs(current - target) < abs(sum - target):
+                    sum = current
+
+        return sum
 
 
-print(Solution().threeSumClosest([-1, 2, 1, -4], 1))
+# print(Solution().threeSumClosest([-1, 2, 1, -4], 1))
 
 # @lc code=end
- 
