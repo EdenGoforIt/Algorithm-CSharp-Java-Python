2f3b2cee5fb6792216352330b61b9923659a81c2
EdenPark <edengoforit@gmail.com>
2022-05-25 20:36:11 +1200

    Verifying an alien dictionary

---
+++ 953.verifying-an-alien-dictionary.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=953 lang=python3
+#
+# [953] Verifying an Alien Dictionary
+#
+
+# @lc code=start
+
+from typing import List
+
+
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        # for index, something in enumerate(order)
+        ind = {c: i for i, c in enumerate(order)}
+        words = [[ind[c] for c in w] for w in words] 
+        return all(w1 <= w2 for w1, w2 in zip(words, words[1:]))
+
+
+print(Solution().isAlienSorted(
+    ["hello", "leetcode"], "hlabcdefgijkmnopqrstuvwxyz"))
+# @lc code=end
