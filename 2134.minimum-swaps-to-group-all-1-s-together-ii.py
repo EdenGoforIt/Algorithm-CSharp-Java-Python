857ffc3a10d4dc96bc7e277d6bc86adee9062f2b
EdenPark <edengoforit@gmail.com>
2022-11-22 08:09:29 +1300

    min swap required

---
+++ 2134.minimum-swaps-to-group-all-1-s-together-ii.py
@@ -0,0 +1,39 @@
+#
+# @lc app=leetcode id=2134 lang=python3
+#
+# [2134] Minimum Swaps to Group All 1's Together II
+#
+from typing import List
+
+# @lc code=start  
+
+class Solution:
+    def minSwaps(self, nums: List[int]) -> int:
+        # get the total of 1's in the list
+        # this will be the window size
+        window_size = nums.count(1)
+
+        # get the first window size and calculate zeros
+        # the number of zeros will be the number of minimum swap is required
+        min_swap = current_zeros = nums[:window_size].count(0)
+
+        for end_index in range(window_size, len(nums)):
+            if nums[end_index - window_size] == 0:
+                current_zeros -= 1
+
+            if nums[end_index] == 0:
+                current_zeros += 1
+            min_swap = min(current_zeros, min_swap)
+
+        return min_swap
+
+
+print(Solution().minSwaps([1, 0, 1, 0, 1]))
+
+# print(Solution().minSwaps([1, 0, 0, 1]))
+# print(Solution().minSwaps([1, 0, 1]))
+# print(Solution().minSwaps([1, 0, 1, 0]))
+# print(Solution().minSwaps([1, 0, 0, 1]))
+# print(Solution().minSwaps([1, 0, 0, 0, 1]))
+
+# @lc code=end
--- 953.verifying-an-alien-dictionary.py
+++ 953.verifying-an-alien-dictionary.py
@@ -13,6 +13,8 @@ class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
         # for index, something in enumerate(order)
         ind = {c: i for i, c in enumerate(order)}
+        test = {c: i for i, c in enumerate(["One", "Two"])}
+        print(test)
         words = [[ind[c] for c in w] for w in words] 
         return all(w1 <= w2 for w1, w2 in zip(words, words[1:]))
 
