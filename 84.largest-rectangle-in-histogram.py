f0c0ef3d683e48aa04659a750c04a7be7a7a7e4a
EdenPark <edengoforit@gmail.com>
2022-12-05 08:12:13 +1300

    Largest rectangle histagram

--- 84.largest-rectangle-in-histogram.py
+++ 84.largest-rectangle-in-histogram.py
@@ -7,10 +7,27 @@
 # @lc code=start
 from typing import List
 
+# https://gist.github.com/jacksongabbard/9cc21485c5e02d145203fa28651f42c4
+
 
 class Solution:
-    def largestRectangleArea(self, heights: List[int]) -> int:
-        # length of the array * min will be width
-        
-# @lc code=end
 
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        heights.append(0)  # add zero on the end
+        # initialize by -1. This is a reference to the zero that we have added
+        stack = [-1]
+        ans = 0
+        for i in range(len(heights)):
+            # -1 will be the last element in the list, so we are starting from zero
+            while heights[i] < heights[stack[-1]]: # if descending order like 3,2
+                h = heights[stack.pop()]
+                w = (i - stack[-1])-1
+                ans = max(ans, h * w)
+            stack.append(i)
+        heights.pop()
+        return ans
+
+        # @lc code=end
+
+
+Solution().largestRectangleArea([2, 1, 4, 3])
