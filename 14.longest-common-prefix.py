a3dbb1969eb707818c260fceceee9ac4dd7c3678
EdenPark <edengoforit@gmail.com>
2022-12-09 08:16:39 +1300

    vertical scannigng lognest comong prefix

--- 14.longest-common-prefix.py
+++ 14.longest-common-prefix.py
@@ -9,9 +9,23 @@ from typing import List
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-
+        # https://leetcode.com/problems/longest-common-prefix/solutions/127449/longest-common-prefix/
         # vertical scanning
+        if strs is None:
+            return ""
+        if len(strs) == 1:
+            return strs[0]
 
+        word = strs[0]
+
+        for i in range(len(word)):
+            c = word[i]
+
+            for w in strs[1:]:
+                if i == len(w) - 1 or w[i] != c:
+                    return word[:i]
+
+        return ""
 
         # horizontal scanning
         # if strs is None:
@@ -24,10 +38,8 @@ class Solution:
 
         #         if not prefix:
         #             return ""
-
         # return prefix
-
-
-print(Solution().longestCommonPrefix(["flower", "flow", "flight"]))
+print(Solution().longestCommonPrefix(["flower", "flower", "flower", "flower"]
+                                     ))
 
 # @lc code=end
