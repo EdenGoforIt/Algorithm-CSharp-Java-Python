e610022ab8c097786b880517885f0fb59ffe6572
EdenPark <edengoforit@gmail.com>
2022-12-11 12:47:26 +1300

    logst comon prefix with binary search

--- 14.longest-common-prefix.py
+++ 14.longest-common-prefix.py
@@ -10,36 +10,59 @@ from typing import List
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         # https://leetcode.com/problems/longest-common-prefix/solutions/127449/longest-common-prefix/
-        # vertical scanning
         if strs is None:
-            return ""
+            ""
         if len(strs) == 1:
             return strs[0]
+        min_length = len(min(strs, key=len))
 
-        word = strs[0]
+        low = 1
+        high = min_length
 
-        for i in range(len(word)):
-            c = word[i]
+        while low <= high:
+            mid = (low+high)//2
+            if self.isCommonPrefix(strs, mid):
+                low = mid + 1
+            else:
+                high = mid - 1
 
-            for w in strs[1:]:
-                if i == len(w) - 1 or w[i] != c:
-                    return word[:i]
+        return strs[0][0: (low+high)//2]
 
-        return ""
+    def isCommonPrefix(self, strs: List[str], mid: int) -> bool:
+        candidate = strs[0][0: mid]
+
+        for i in range(len(strs)):
+            if not strs[i].startswith(candidate):
+                return False
+
+        return True
+        # vertical scanning
+        # if strs is None:
+        #     return ""
+        # if len(strs) == 1:
+        #     return strs[0]
+
+        # word = strs[0]
+
+        # for i in range(len(word)):
+        #     c = word[i]
+
+        #     for w in strs[1:]:
+        #         if i == len(w) - 1 or w[i] != c:
+        #             return word[:i]
+
+        # return ""
 
         # horizontal scanning
         # if strs is None:
         #     return ""
-
         # prefix = strs[0]
         # for i in range(1, len(strs)):
         #     while prefix not in strs[i]:
         #         prefix = prefix[0: len(prefix)-1]
-
         #         if not prefix:
         #             return ""
         # return prefix
-print(Solution().longestCommonPrefix(["flower", "flower", "flower", "flower"]
-                                     ))
+# print(Solution().longestCommonPrefix(["flower", "flow", "flaw"]))
 
 # @lc code=end
