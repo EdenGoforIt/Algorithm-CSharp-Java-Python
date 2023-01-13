5ad45982e865c2a5bbb5606c1d6708bb25dff71d
EdenPark <edengoforit@gmail.com>
2023-01-13 20:50:25 +1300

    finding max profit another way

--- python/p18-1-maxprofit.py
+++ python/p18-1-maxprofit.py
@@ -2,17 +2,26 @@ from typing import List
 
 
 def find_max_profit_brute_force(prices: List[str]) -> int:
+    min = prices[0]
     max_profit = 0
-
-    for i in range(0, len(prices)-1):
-        for j in range(i+1, len(prices)):
-            profit = prices[j] - prices[i]
-            if profit > max_profit:
-                max_profit = profit
+    for i in range(1, len(prices)):
+        profit = prices[i] - min
+        if profit > max_profit:
+            max_profit = profit
+        if prices[i] < min:
+            min = prices[i]
 
     return max_profit
 
+    # max_profit = 0
 
+    # for i in range(0, len(prices)-1):
+    #     for j in range(i+1, len(prices)):
+    #         profit = prices[j] - prices[i]
+    #         if profit > max_profit:
+    #             max_profit = profit
+
+    # return max_profit
 stock = [10300, 9600, 9800, 8200, 7800, 8300, 9500, 9800, 10200, 9500]
 
 print(find_max_profit_brute_force(stock))
