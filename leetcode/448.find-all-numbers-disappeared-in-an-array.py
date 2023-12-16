26a874519a0e1c87df4f2b7d6f0d17f7a67a1ea6
Eden Park <edengoforit@gmail.com>
2023-12-16 20:49:18 +1300

    finding all number disappeared

--- leetcode/448.find-all-numbers-disappeared-in-an-array.py
+++ leetcode/448.find-all-numbers-disappeared-in-an-array.py
@@ -4,9 +4,21 @@
 # [448] Find All Numbers Disappeared in an Array
 #
 
+
 # @lc code=start
+from typing import List
+
+
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        
-# @lc code=end
+        set_num = set(nums)
+        result = []
+        for i in range(1, len(nums) + 1):
+            if i not in set_num:
+                result.append(i)
 
+        return result
+
+
+Solution().findDisappearedNumbers([1, 1])
+# @lc code=end
