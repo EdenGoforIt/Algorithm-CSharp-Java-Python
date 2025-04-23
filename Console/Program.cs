a917abb3df2d02b57245c33118bf6d3ec3e6581c
Eden Park <edengoforit@gmail.com>
2025-04-23 20:01:53 +1200

    feat: change console app to main method

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Algorithm_CSharp_Java_Python.ConsoleApp;
+
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Hello, World!22");
+	}
+}
--- Console/obj/Debug/net9.0/Console.AssemblyInfo.cs
+++ Console/obj/Debug/net9.0/Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e871e1f5b9286ecdd1e5d237e4f0a273a82a44a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d00f60d3a7d31ce8eb9ad04c1c2e0b6ae95e4f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
