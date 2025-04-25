48a8594a4232ee7923c1e8210bbafaef6149e1e7
Eden Park <edengoforit@gmail.com>
2025-04-25 21:05:14 +1200

    feat: create method

--- Console/Program.cs
+++ Console/Program.cs
@@ -5,12 +5,18 @@ namespace Algorithm_CSharp_Java_Python.ConsoleApp;
 
 class Program
 {
-	const int[] prices = new int[] { 7, 1, 5, 3, 6, 4 };
-	static void Main(string[] args = args)
+	static readonly int[] prices = new int[] { 7, 1, 5, 3, 6, 4 };
+	static void Main(string[] args)
 	{
 
-
+		int result = MaxProfit(prices);
 
 		Console.WriteLine($"Hello, World {prices.Length}");
+	}
+
+	static int MaxProfit(int[] prices)
+	{
+
+		return 0;
 	}
 }
