0c54f61a395062682dcfb32a87b57268ac05cecb
Eden Park <edengoforit@gmail.com>
2025-05-02 19:54:34 +1200

    feat: add numarray

---
+++ leetcode/303.range-sum-query-immutable.cs
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=303 lang=csharp
+ *
+ * [303] Range Sum Query - Immutable
+ */
+
+// @lc code=start
+public class NumArray
+{
+
+	public NumArray(int[] nums)
+	{
+
+	}
+
+	public int SumRange(int left, int right)
+	{
+
+	}
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.SumRange(left,right);
+ */
+// @lc code=end
+
