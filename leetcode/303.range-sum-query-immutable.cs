c2ec8b46a1b518b618b98fc5473b4da0c81c5939
Eden Park <edengoforit@gmail.com>
2025-06-03 19:29:43 +1200

    feat: 303 leet

--- leetcode/303.range-sum-query-immutable.cs
+++ leetcode/303.range-sum-query-immutable.cs
@@ -4,18 +4,31 @@
  * [303] Range Sum Query - Immutable
  */
 
+// Input
+//["NumArray","sumRange","sumRange","sumRange"]
+//[[[-2,0,3,-5,2,-1]],[0,2],[2,5],[0,5]]
+
 // @lc code=start
+using System.Reflection.Emit;
+using Internal;
+
 public class NumArray
 {
-
+	private int[] sums;
 	public NumArray(int[] nums)
 	{
+		sums = new int[nums.Length + 1];
 
+		for (var i = 0; nums.Length; i++)
+		{
+			sums[i] = sums[i - 1] + sums[i];
+			Console.WriteLine($"sums[i]", sums[i]);
+		}
 	}
 
 	public int SumRange(int left, int right)
 	{
-
+		return sums[right] - sums[left];
 	}
 }
 
