bc84628f614a4dceede298c98227d5f3d8e4973a
Eden Park <edengoforit@gmail.com>
2025-08-23 18:43:00 +1200

    feat: 46 permutation

---
+++ leetcode/46.permutations.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=46 lang=python3
+#
+# [46] Permutations
+#
+
+# @lc code=start
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+# @lc code=end
+
