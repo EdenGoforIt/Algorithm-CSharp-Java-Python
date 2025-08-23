73871bdbc9618cfd2fcf69538a9eefc92b723b60
Eden Park <edengoforit@gmail.com>
2025-08-24 09:12:28 +1200

    feat: more comment on permutation

--- leetcode/46.permutations.py
+++ leetcode/46.permutations.py
@@ -3,10 +3,27 @@
 #
 # [46] Permutations
 #
+from typing import List
+
 
 # @lc code=start
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        
-# @lc code=end
+        if len(nums) == 0:
+            return [[]]
+        answer = []
+        for i, current in enumerate(nums):
+            # with [1,2,3], this will return [2,3]
+            remaining = nums[:i] + nums[i + 1 :]
+            # here we look [2,3]
+            # if 2, then
+            for p in self.permute(remaining):
+                # with [2,3], this will return [[2,3], [3,2]]
+                answer.append([current] + p)
+        print(answer)
+        return answer
 
+
+if __name__ == "__main__":
+    # Example test case
+    Solution().permute([1, 2, 3])
