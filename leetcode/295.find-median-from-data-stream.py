7d05cacc95a6ac5b30c0f386bcb3f255dcf99061
Eden Park <edengoforit@gmail.com>
2025-09-17 07:38:27 +1200

    feat: find medium

--- leetcode/295.find-median-from-data-stream.py
+++ leetcode/295.find-median-from-data-stream.py
@@ -23,19 +23,23 @@ class MedianFinder:
 
     def addNum(self, num: int) -> None:
         # heapq only supports min heap
-        # if
         if not self.small or num <= -self.small[0]:
             heapq.heappush(self.small, -num)
         else:
             heapq.heappush(self.large, num)
 
         # Balance the heaps
+        # if max heap has more number then add it to min heap
+        # we are trying to save if odd number, max heap will have more number
+        # 1,2 min heap and 3,4,5 max heap (small)
         if len(self.small) > len(self.large) + 1:
             heapq.heappush(self.large, -heapq.heappop(self.small))
         elif len(self.large) > len(self.small):
             heapq.heappush(self.small, -heapq.heappop(self.large))
 
     def findMedian(self) -> float:
+        # so that if max heap has more we return mean it's. odd number 
+        # and max heap has more number.  
         if len(self.small) > len(self.large):
             return -self.small[0]
         return (-self.small[0] + self.large[0]) / 2
