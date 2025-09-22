8749899af9145abbb1d28d047ee33281008f7c2e
Eden Park <edengoforit@gmail.com>
2025-09-22 19:37:48 +1200

    feat: 121

---
+++ leetcode/121.best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=121 lang=python3
+#
+# [121] Best Time to Buy and Sell Stock
+#
+from typing import List
+
+# @lc code=start
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
+        max_profit = 0
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
+        return max_profit
+        
+# @lc code=end
+
