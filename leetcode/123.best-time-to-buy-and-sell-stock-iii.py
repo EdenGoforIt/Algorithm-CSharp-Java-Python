e575c87f134a609cf1a34f76799dd129add38f7c
Eden Park <edengoforit@gmail.com>
2025-09-29 10:14:33 +1300

    rewirting from scratch 123

--- leetcode/123.best-time-to-buy-and-sell-stock-iii.py
+++ leetcode/123.best-time-to-buy-and-sell-stock-iii.py
@@ -13,20 +13,12 @@ class Solution:
         sell1, sell2 = 0, 0
         # [7, 1, 5, 3]
         for price, i in enumerate(prices):
-            # -inf < -7 => buy1 = -7
-            if buy1 < -price:
-                buy1 = -price
-                print(f"First buy on day {i} at price {price}")
+            # We start by making a debt of the price as we are buying the stock
+            # with max we compare which is better to buy at the cheaper price
+            buy1 = max(buy1, -price)
+            
 
-            if sell1 < buy1 + price:
-                sell1 = buy1 + price
-                print(f"First sell on day {i} at price {price} → profit {sell1}")
-            if buy2 < sell1 - price:
-                buy2 = sell1 - price
-                print(f"Second buy on day {i} at price {price} → profit {buy2}")
-            if sell2 < buy2 + price:
-                sell2 = buy2 + price
-                print(f"Second sell on day {i} at price {price} → profit {sell2}")
+        
         print(f"Max profit: {sell2}")
         return sell2
 
