2827718f01d0b8c37d3e735ff320fe13d96f2e54
Eden Park <edengoforit@gmail.com>
2025-09-29 20:58:07 +1300

    feat: 268 added

---
+++ leetcode/268.missing-number.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=268 lang=python3
+#
+# [268] Missing Number
+#
+from typing import List
+# @lc code=start
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        
+# @lc code=end
+
