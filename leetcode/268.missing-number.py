804f9347197bf3fbeb09f6cc8b7f9ed41acac9b9
Eden Park <edengoforit@gmail.com>
2025-10-02 21:20:17 +1300

    feat: 268 xor way

--- leetcode/268.missing-number.py
+++ leetcode/268.missing-number.py
@@ -9,10 +9,14 @@ from typing import List
 # @lc code=start
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        expected_sum = n * (n + 1) // 2
-        actual_sum = sum(nums)
-        return expected_sum - actual_sum
+        # n = len(nums)
+        # expected_sum = n * (n + 1) // 2
+        # actual_sum = sum(nums)
+        # return expected_sum - actual_sum
+        res = len(nums)
+        for i, num in enumerate(nums):
+            res ^= i ^ num
+        return res
 
 
 # @lc code=end
