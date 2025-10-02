81645024c8d38efcd0b51f92931fda1b45b3e54d
Eden Park <edengoforit@gmail.com>
2025-10-02 21:24:07 +1300

    feat: let'sdo 136 again

--- leetcode/136.single-number.py
+++ leetcode/136.single-number.py
@@ -12,10 +12,7 @@ from typing import List
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        result = 0
-        for num in nums:
-            result ^= num
-        return result
+ 
  
         
 # @lc code=end
