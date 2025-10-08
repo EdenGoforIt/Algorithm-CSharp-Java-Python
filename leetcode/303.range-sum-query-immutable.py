48b46688a1a4a640d2026b935442a1a221a59a76
Eden Park <edengoforit@gmail.com>
2025-10-08 20:42:18 +1300

    303

--- leetcode/303.range-sum-query-immutable.py
+++ leetcode/303.range-sum-query-immutable.py
@@ -9,6 +9,10 @@ from typing import List
 class NumArray:
 
     def __init__(self, nums: List[int]):
+        self.sum = [0] * (len(nums) + 1)
+        for i in range(len(nums)):
+            self.sum[i + 1] = self.sum[i] + nums[i]
+        
 
         
 
